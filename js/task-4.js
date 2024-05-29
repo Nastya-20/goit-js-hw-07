677cc8af1bf03d39daa2ab5919e791f90eda6983
Nastya-20 <62868214+Nastya-20@users.noreply.github.com>
2024-05-29 23:13:55 +0300

    update

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,32 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+  
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+  
+    if (!email || !password) {
+       alert('All form fields must be filled in');
+    return;
+  }
+  
+   const formData = {
+    email: email,
+    password: password,
+  };
+  const input = document.querySelector('#name-input');
+
+input.addEventListener('focus', () => {
+  input.setAttribute('placeholder', 'Type area');
+});
+   console.log(formData);
+  
+    form.reset();
+    
+});
+
+
+
+
 
